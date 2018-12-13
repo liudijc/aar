b9d87ef6fc329b0083a09b635fefbeb975acf414
liudijc <45677983+liudijc@users.noreply.github.com>
2018-12-13 18:06:49 +0800

    ok
    ok

--- beif/DCKHD/name.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def get_names(a):
-    try:
-        list = [1, 2, 3,4,5,6,7,8,9]
-        c = random.sample(list,int(a) )
-        return c
-    except Exception as e:
-        return e; 
